3dff52001e902654a6aa6814577941b0a8bb1067
Michael Kutz <michael.kutz@rewe-digital.com>
2015-11-13 21:19:48 +0100

    #1 changed test setup to random port number.

--- src/test/groovy/com/rewe/digital/staff/test/ProfileServiceSpec.groovy
+++ src/test/groovy/com/rewe/digital/staff/test/ProfileServiceSpec.groovy
@@ -1,16 +1,18 @@
 package com.rewe.digital.staff.test
 import com.rewe.digital.staff.Application
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.SpringApplicationContextLoader
 import org.springframework.boot.test.WebIntegrationTest
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.web.client.RestTemplate
 import spock.lang.Specification
 
-@WebIntegrationTest(randomPort = false)
+@WebIntegrationTest(randomPort = true)
 @ContextConfiguration(loader = SpringApplicationContextLoader, classes = Application)
 class ProfileServiceSpec extends Specification {
 
-    String resourceUrl = "http://localhost:8080/api/profiles"
+    @Value("http://localhost:\${local.server.port}/api/profiles")
+    String resourceUrl
 
     RestTemplate rest = new RestTemplate()
 
